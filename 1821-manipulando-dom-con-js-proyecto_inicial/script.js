0bad49c518395f4557f6f3c14aefe6a64822f85f
Oskar Lopez <puntoexactogeek@gmail.com>
2022-12-12 15:17:25 -0500

    clase05 terminada

--- 1821-manipulando-dom-con-js-proyecto_inicial/script.js
+++ 1821-manipulando-dom-con-js-proyecto_inicial/script.js
@@ -1,4 +1,6 @@
-(( ) => {
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
+
 const btn = document.querySelector("[data-form-btn]");
 
 const createTask = (evento) => {
@@ -10,39 +12,18 @@ const createTask = (evento) => {
   task.classList.add("card");
   input.value = "";
   const taskContent = document.createElement("div");
-  
+
   const titleTask = document.createElement("span");
   titleTask.classList.add("task");
   titleTask.innerText = value;
   taskContent.appendChild(checkComplete());
   taskContent.appendChild(titleTask);
 
-  const content = `
-  <i class="fas fa-trash-alt trashIcon icon"></i>`;
-
   //task.innerHTML = content;
+
   task.appendChild(taskContent);
-
+  task.appendChild(deleteIcon());
   list.appendChild(task);
-
-  console.log(content);
 };
-
 
 btn.addEventListener("click", createTask);
-
-const checkComplete = () => {
-  const i = document.createElement("i");
-  i.classList.add("far", "fa-check-square", "icon");
-  i.addEventListener("click", completeTask);
-  return i;
-};
-
-//IIFE, funciones en cuanto se declaran se ejecutan
-const completeTask = (event) =>{
-  const element = element.target
-  element.classList.toggle("fas");
-  element.classList.toggle("completeIcon");
-  element.classList.toggle("far");
-};
-})();
